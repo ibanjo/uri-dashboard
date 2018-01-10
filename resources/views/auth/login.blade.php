0722e43f886634649e54fb50348ccdaa34f45cc0
Giuseppe Maria D'Aucelli <giuseppe.daucelli@gmail.com>
2018-01-10 12:11:32 +0100

    Release for first demo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,7 +16,7 @@
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <label for="email" class="col-md-4 control-label">E-Mail</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
@@ -47,7 +47,7 @@
                             <div class="col-md-6 col-md-offset-4">
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Ricordami
                                     </label>
                                 </div>
                             </div>
@@ -60,7 +60,7 @@
                                 </button>
 
                                 <a class="btn btn-link" href="{{ route('forgotten_password') }}">
-                                    Forgot Your Password?
+                                    Password dimenticata?
                                 </a>
                             </div>
                         </div>
